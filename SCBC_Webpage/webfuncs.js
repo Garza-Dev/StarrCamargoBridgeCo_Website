3a70d576ffd40cfd3d889d79bce31f653d341b5f
Garza-Dev <thecaptlespro@gmail.com>
2022-07-15 10:05:54 -0500

    StyleChanges&ResourcesAdded

--- SCBC_Webpage/webfuncs.js
+++ SCBC_Webpage/webfuncs.js
@@ -1,23 +1,23 @@
 function baluarteBtn() {
-    document.getElementById("bg_img_infobox").style.backgroundImage = "url(images&videos/PICS/BaluarteBridge3.jpg)";
+    document.getElementById("bg_img_infobox").style.backgroundImage = "url(images&videos/PICS/BaluarteBridge.png)";
     document.getElementById("infobox_txt_h1").textContent = "Baluarte Bridge";
     document.getElementById("infobox_txt_p").textContent = "The Mazatlán-Durango highway project is the last link in a transversal inter-oceanic corridor connecting Mexico's Pacific Coast with the Gulf of Mexico. Connecting Mexico's Pacific Coast with The Gulf of Mexico.";
 }
 
 function loopBtn() {
-    document.getElementById("bg_img_infobox").style.backgroundImage = "url(images&videos/PICS/truck02.jpg)";
+    document.getElementById("bg_img_infobox").style.backgroundImage = "url(images&videos/PICS/TRUCKS.png)";
     document.getElementById("infobox_txt_h1").textContent = "Loop 195";
     document.getElementById("infobox_txt_p").textContent = "Loop 195 will provide more efficient highway connectivity with Laredo to the West and prevent damage by heavy truck traffic. Connecting Laredo to The West.";
 }
 
 function shortcutBtn() {
-    document.getElementById("bg_img_infobox").style.backgroundImage = "url(images&videos/PICS/MX-USA.jpg)";
+    document.getElementById("bg_img_infobox").style.backgroundImage = "url(images&videos/PICS/MX-USA.png)";
     document.getElementById("infobox_txt_h1").textContent = "The Shortcut";
     document.getElementById("infobox_txt_p").textContent = "The Starr Camargo Bridge serves as the most direct and efficient route between the Rio Grande Valley and Mexican cities such as Monterrey and Mexico City. The two-lane bridge, established in 1966, is 591ft long. The crossing is also known locally as Puente Camargo. The bridge is privately owned by the Starr Camargo Bridge Company.";
 }
 
 function demoBtn() {
-    document.getElementById("bg_img_infobox").style.backgroundImage = "url(images&videos/PICS/MX-US-CAN2.jpg)";
+    document.getElementById("bg_img_infobox").style.backgroundImage = "url(images&videos/PICS/MX-USA-CAN.png)";
     document.getElementById("infobox_txt_h1").textContent = "Demographics";
     document.getElementById("infobox_txt_p").textContent = "South Texas is booming with a great trade economy, people relocating from other parts of the country, and tourism from Mexico. Free trade agreements between Mexico the US and Canada have made this port essential to our country's economy.";
 }
